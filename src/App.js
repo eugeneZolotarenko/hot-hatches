2ab9af4d1835d7c469a0e15fe856e3f02bece69b
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-02-05 21:30:09 +0100

    Adding createGlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { createGlobalStyle } from 'styled-components';
 import Router from './Router';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #FCFFF3;
+    margin: 0;
+    padding: 0;
+  }
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <Router/>
-    </div>
+        <div className="App">
+            <GlobalStyle />
+            <Router/>
+        </div>
   );
 }
 
