6b6ca729bc4310e9885c4fd67afe0a3f9fcb376b
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-02-08 16:26:27 +0100

    Reverting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+
 import Router from './Router';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #FCFFF3;
-    margin: 0;
-    padding: 0;
-  }
-`
-
 
 function App() {
   return (
-        <div className="App">
-            <GlobalStyle />
-            <Router/>
-        </div>
+    <div className="App">
+      <Router/>
+    </div>
   );
 }
 
