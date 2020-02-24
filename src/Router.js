49e0b55cc53db1878977789f0eab2784ebdbe8c3
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-02-24 19:18:52 +0100

    Developing aplication logic

--- src/Router.js
+++ src/Router.js
@@ -8,28 +8,70 @@ import Brand from './pages/Brand';
 
 const Router = () => {
   const [data, setData] = useState({ hatches: [] });
+  const [model, setModel] = useState('S 3');
+  const [modelData, setModelData] = useState();
 
   useEffect(() => {
     async function listModels(brand) {
       const snapshot = await firestore.collection(brand).get();
       const dataArr = []
       snapshot.docs.forEach(doc => {
-      const data = doc.data();
-      dataArr.push(data);
+        const data = doc.data();
+        dataArr.push(data);
       });
       await setData(dataArr);
+    
     };
     listModels('Audi');
   }, []);
 
-  console.log(data);
+  function hasValue(obj, key, value) {
+    return obj.hasOwnProperty(key) && obj[key] === value;
+  }
 
-  return (
-  <Switch>
-    <Route exact path='/' component={Main} />
-    <Route exact path='/brand' component={Brand} />
-  </Switch>
-  );
+  useEffect(() => {
+    if (data.length > 0 && model !== undefined) {
+      function getHatchData(value, key = 'name') {
+        data.filter(obj => {
+          if (hasValue(obj, key, value)) {
+            setModelData(obj);
+          } else {
+            return null
+          }
+        });
+      }
+      getHatchData(model)
+    }
+
+   }, [data, model]);
+
+   useEffect(() => {
+    if (modelData !== undefined) {
+      console.log(modelData)
+    } else {
+      console.log('ooops')
+    }
+
+  }, [modelData]);
+
+  if (data.length > 0) {
+    return (
+      <Switch>
+        {/* <div>
+        <button onClick={handleClick}>RS 3</button>
+        <button onClick={handleClick}>S 3</button>
+        </div> */}
+        <Route exact path='/'
+        render={(props) => <Main {...props} model={model} />}
+        />
+        <Route exact path='/brand' component={Brand} />
+      </Switch>
+    );   
+  } else {
+   return (
+      <p>Loading...</p>
+   )
+  }
 }
 
 export default Router;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 import MainHeader from '../../components/MainHeader';
 import MainStyle from './MainStyle';
 
-const Main = () => {
+const Main = (props) => {
+
+   function handleClick(e) {
+    //  const hatchModel = e.target.textContent;
+    //  setModel(hatchModel);
+    console.log(props.model)
+   }
+
     return (
         <MainStyle>
+            <div>
+                <button onClick={handleClick}>RS 3</button>
+                <button onClick={handleClick}>S 3</button>
+            </div>
             <MainHeader/>
         </MainStyle>
     )
