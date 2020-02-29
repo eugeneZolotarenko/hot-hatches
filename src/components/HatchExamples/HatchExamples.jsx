b289717c32ce485199ac7fad00213557f4285e6a
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-02-29 17:49:20 +0100

    Creating Conception of HatchExamples component

--- src/components/HatchExamples/HatchExamples.jsx
+++ src/components/HatchExamples/HatchExamples.jsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import history from "../../history"
+
+import firestore from "../../firebase/firebase.utils"
 
 import HatchExamplesStyle from "./HatchExamplesStyle"
 
 const HatchExamples = (props) => {
+  const [examples, setExamples] = useState([])
   // function handleClick(e) {
   //   const hatchBrand = e.target.textContent
   //   async function redirectToPage() {
@@ -13,11 +16,43 @@ const HatchExamples = (props) => {
   //   }
   //   redirectToPage()
   // }
+
+  const collection = "Examples"
+
+  useEffect(() => {
+    async function getExamples(collection) {
+      const snapshot = await firestore.collection(collection).get()
+      const dataArr = []
+      snapshot.docs.forEach((doc) => {
+        const data = doc.data()
+        dataArr.push(data)
+      })
+      setExamples(dataArr)
+    }
+    getExamples(collection)
+  }, [])
+
+  console.log(examples)
+
   return (
     <HatchExamplesStyle>
-      {/* <div>
-        <button onClick={handleClick}>Audi</button>
-      </div> */}
+      <div>
+        {examples.map((hatch, i) => {
+          return (
+            <button
+              key={i}
+              data-brand={hatch.brand}
+              data-model={hatch.model}
+              style={{
+                backgroundImage: `url('assets/${hatch.brand}/${hatch.model}.jpg')`
+              }}>
+              <span>
+                {hatch.brand} {hatch.model}
+              </span>
+            </button>
+          )
+        })}
+      </div>
     </HatchExamplesStyle>
   )
 }
