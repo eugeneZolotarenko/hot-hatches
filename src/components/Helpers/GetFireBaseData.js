1002e4d3455d274d5e94cff67881bf47df8817ba
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-03-07 12:12:56 +0100

    Adding id to objeacts of data

--- src/components/Helpers/GetFireBaseData.js
+++ src/components/Helpers/GetFireBaseData.js
@@ -1,11 +1,16 @@
 import firestore from "../../firebase/firebase.utils"
 
+function addDocIdToArr(doc, array) {
+  array.forEach((obj) => (obj.id = doc.id))
+}
+
 async function GetFireBaseData(collection, setData) {
   const snapshot = await firestore.collection(collection).get()
   const dataArr = []
   snapshot.docs.forEach((doc) => {
     const data = doc.data()
     dataArr.push(data)
+    addDocIdToArr(doc, dataArr)
   })
   setData(dataArr)
 }
