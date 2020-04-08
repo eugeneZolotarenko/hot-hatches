11cafe8373f466ae7e60e1c1827c984e932fa23a
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-04-08 19:45:45 +0200

    Styling desktop version of Spec block

--- src/components/Model/Specs/Specs.jsx
+++ src/components/Model/Specs/Specs.jsx
@@ -10,7 +10,7 @@ const Specs = ({ modelData }) => {
           <span className='first-letter model-first-letter'>S</span>Specs
         </h3>
       </div>
-      <div className='specs-container'>
+      <div className='specs-container dark-container'>
         <ul className='specs-wrapper'>
           <li className='spec'>Engine: {modelData.engine}</li>
           <li className='spec'>Power: {modelData.power}</li>
@@ -24,10 +24,10 @@ const Specs = ({ modelData }) => {
           <li className='spec'>Weight: {modelData.weight}</li>
         </ul>
       </div>
-      <div className='specs-container'>
+      <div className='specs-container yellow-container'>
         <ul className='specs-wrapper'>
           <li className='spec'>Max. Speed: {modelData.maxSpeed}</li>
-          <li className='spec'>0-100 km/h: {modelData.sero100}</li>
+          <li className='spec'>0-100 km/h: {modelData.zero100}</li>
           <li className='spec'>1/4 mile: {modelData.quarterMile}</li>
           <li className='spec'>
             {modelData.nurburgring
--- src/components/Model/Specs/SpecsStyle.js
+++ src/components/Model/Specs/SpecsStyle.js
@@ -2,13 +2,18 @@ import styled from "styled-components"
 
 const SpecsStyle = styled.section`
   max-width: 2000px;
+  display: flex;
+  position: relative;
   .header-container {
-    max-width: 17vw;
+    max-width: 17%;
     min-height: 150px;
+    max-height: 160px;
+    min-width: 15%;
     display: flex;
     align-items: center;
     justify-content: flex-end;
     background: linear-gradient(180deg, #fcfff3 50%, #f3b81d 50%);
+    margin-right: 10vw;
 
     h3 {
       font-size: 5.2rem;
@@ -23,6 +28,39 @@ const SpecsStyle = styled.section`
         transform: scale(0.8);
       }
     }
+  }
+
+  .specs-container {
+    width: fit-content;
+    margin-top: 120px;
+    .specs-wrapper {
+      list-style-type: none;
+      padding: 45px 30px;
+      margin: 0;
+      .spec {
+        font-size: 2.2rem;
+        line-height: 1.8;
+        font-weight: 500;
+      }
+    }
+  }
+
+  .dark-container {
+    background: ${(props) => props.theme.nightBlue};
+    min-width: 18%;
+    max-width: 18%;
+    height: 300px;
+    .specs-wrapper {
+      .spec {
+        color: white;
+      }
+    }
+  }
+
+  .yellow-container {
+    width: 35%;
+    height: 250px;
+    background-color: ${(props) => props.theme.yellow};
   }
 `
 
